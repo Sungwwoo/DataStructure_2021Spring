86ddbf4f720d191969172d8a9a1ddb29d7f2564a
sungwwoo <p11120076@gmail.com>
2021-03-12 18:46:07 +0900

    Lab02

--- Lab/Lab02/1/GenerateText/create.cpp
+++ Lab/Lab02/1/GenerateText/create.cpp
@@ -4,15 +4,15 @@ using namespace std;
 int main(){
 
 	ofstream output_file1("unix_LF.txt");
-	output_file1 << "Hello\nWorld\nby Sungwoo.";
+	output_file1 << "Hello\nWorld";
 	output_file1.close();
 
 	ofstream output_file2("mac_CR.txt");
-	output_file2 << "Hello\rWorld\rby Sungwoo.";
+	output_file2 << "Hello\rWorld";
 	output_file2.close();
 
 	ofstream output_file3("windows_CRLF.txt");
-	output_file3 << "Hello\r\nWorld\r\nby Sungwoo.";
+	output_file3 << "Hello\r\nWorld\r\n";
 	output_file3.close();
 	return 0;
 }
